232a66823326aa8aaa6f8c667d0382c9936064ab
Massimo Cannavo <massimo.wo6q2@simplelogin.co>
2023-04-28 22:24:42 -0400

    Implemented checking of root user

---
+++ lfs/utils.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+#
+# Code to be sourced by other scripts.
+
+readonly RED="\033[1;31m"
+readonly GREEN="\033[1;32m"
+readonly YELLOW="\033[1;33m"
+readonly BLUE="\033[1;34m"
+readonly RESET="\033[m"
+
+is_root() {
+  if [[ $EUID -ne 0 ]]; then
+    echo -e "${RED}run as root${RESET}"
+    exit 1
+  fi
+}
