b9d56ea7e4a8f77c13ed261377fd81ea1c40c98c
Massimo Cannavo <massimo.wo6q2@simplelogin.co>
2023-04-30 13:50:18 -0400

    Added checking if script was executed as root

--- scripts/partition.py
+++ scripts/partition.py
@@ -1,10 +1,19 @@
 '''Partitions a device using a YAML file.'''
-import argparse
+from argparse import ArgumentParser
+from dataclasses import dataclass
+import os
+import sys
+
+@dataclass
+class Colors:
+    '''Colors used to diplay to the console.'''
+    RED = '\033[1;31m'
+    RESET = '\033[m'
 
 
 def parse_args():
     '''Parse command line arguments.'''
-    parser = argparse.ArgumentParser(description='Partitions a device from a YAML file.')
+    parser = ArgumentParser(description='Partitions a device from a YAML file.')
     parser.add_argument('-f', '--file',
                         default='../partitions.yaml',
                         help='reads from FILE instead of the default (partitions.yml)',
@@ -20,6 +29,9 @@ def parse_args():
 def main():
     '''The main entrypoint of the script.'''
     args = parse_args()
+    if os.geteuid() != 0:
+        print(f'{Colors.RED}run as root{Colors.RESET}')
+        sys.exit(1)
 
 
 if __name__ == '__main__':
