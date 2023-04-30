35a62fe5a7ede9d3f66d9c6d3014393da88554e2
Massimo Cannavo <massimo.wo6q2@simplelogin.co>
2023-04-29 22:18:28 -0400

    Added python script to partition a device

---
+++ scripts/partition.py
@@ -0,0 +1,23 @@
+'''Partitions a device using a YAML file.'''
+import argparse
+
+
+def parse_args():
+    '''Parse command line arguments.'''
+    parser = argparse.ArgumentParser(description='Partitions a device from a YAML file.')
+    parser.add_argument('-f', '--file',
+                        default='../partitions.yaml',
+                        help='reads from FILE instead of the default (partitions.yml)',
+                        type=str)
+
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    '''The main entrypoint of the script.'''
+    args = parse_args()
+
+
+if __name__ == '__main__':
+    main()
