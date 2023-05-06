08ba2c2c763f9e96202b93611531cb605dd54512
Massimo Cannavo <massimo.wo6q2@simplelogin.co>
2023-05-05 20:26:38 -0400

    Added function for converting size

--- scripts/partition.py
+++ scripts/partition.py
@@ -1,6 +1,7 @@
 '''Partitions a device using a YAML file.'''
 from argparse import ArgumentParser, Namespace
 from dataclasses import dataclass
+import math
 import os
 from pathlib import Path
 import subprocess
@@ -14,6 +15,10 @@ PARENT_DIR = Path(__file__).parent.parent
 CONFIG_FILE = f'{PARENT_DIR}/partitions.yaml'
 SCHEMA_FILE = f'{PARENT_DIR}/partitions-schema.yaml'
 UTF8 = 'utf-8'
+UNIT_SYSTEM = {
+    **dict.fromkeys(['B', 'KiB', 'MiB', 'GiB', 'TiB'], 1024),
+    **dict.fromkeys(['KB', 'MB', 'GB', 'TB'], 1000)
+}
 
 
 @dataclass
@@ -45,7 +50,8 @@ def validate_config(config: dict) -> bool:
     Validates the YAML config file agains a defined schema.
 
     Parameters
-        config: Contents of the YAML config file to validate.
+        config: dict
+            Contents of the YAML config file to validate.
     '''
     path = Path(SCHEMA_FILE)
     if not path.is_file():
@@ -68,7 +74,8 @@ def parse_config(filename: str) -> dict:
     Parse YAML file and extract the config needed to partition the device.
 
     Parameters
-        filename: Name of the YAML config file to parse.
+        filename: str
+            Name of the YAML config file to parse.
     '''
     path = Path(filename)
     if not path.is_file():
@@ -97,7 +104,8 @@ def get_dev_path(serial_id: str) -> str:
     Looks up the device path given a serial ID.
 
     Parameters
-        serial_id: Serial ID of the device.
+        serial_id: str
+            Serial ID of the device.
     '''
     context = pyudev.Context()
     for device in context.list_devices(subsystem='block', DEVTYPE='disk'):
@@ -108,12 +116,65 @@ def get_dev_path(serial_id: str) -> str:
     return None
 
 
+def to_bytes(size: int, size_unit: str) -> int:
+    '''
+    Converts size to bytes from the specified unit.
+
+    Parameters
+        size: int
+            The size to convert to bytes.
+        size_unit: str
+            The unit that the size is being converted from.
+    '''
+    unit = UNIT_SYSTEM.get(size_unit)
+    if not unit:
+        print(f'invalid unit {size_unit}')
+
+    units = {
+        'B': 1,
+        'K': unit,
+        'M': unit**2,
+        'G': unit**3,
+        'T': unit**4
+    }
+
+    return size * units.get(size_unit[0])
+
+
+def convert_size(size_bytes: int, size_unit: str) -> tuple[int, str]:
+    '''
+    Converts bytes into appropriate unit for readability.
+
+    Parameters
+        size_bytes: int
+            Size in bytes to convert to the appropriate unit.
+        size_unit: str
+            The original unit being used to parition the device.
+    '''
+    unit = UNIT_SYSTEM.get(size_unit)
+    if not unit:
+        print(f'invalid unit {size_unit}')
+
+    i = int(math.floor(math.log(size_bytes, unit)))
+    size = int(round(size_bytes / math.pow(unit, i), 2))
+    units = ('B', 'K', 'M', 'G', 'T')
+    if i == 0:
+        suffix = ''
+    elif unit == 1024:
+        suffix = 'iB'
+    elif unit == 1000:
+        suffix = 'B'
+
+    return (size, f'{units[i]}{suffix}')
+
+
 def display_partitions(dev_path: str) -> None:
     '''
     Display partition table of device.
 
     Parameters
-        dev_path: path of device to display partition table.
+        dev_path: str
+            path of device to display partition table.
     '''
     try:
         subprocess.run(['parted', dev_path, 'print'], check=False)
@@ -123,25 +184,41 @@ def display_partitions(dev_path: str) -> None:
 
 
 def display_changes(config: dict, dev_path: str) -> None:
-    '''TODO: add docstring.'''
+    '''
+    Display changes that will be performed on the device.
+
+    Parameters
+        config: dict
+            Config used to partition the device.
+        dev_path: str
+            Path of the device to partition.
+    '''
     print(f'{Colors.YELLOW}Device will be wiped and formatted:{Colors.RESET}\n'
           f'Partition Table: {Colors.BLUE}{config.get("partitionScheme")}{Colors.RESET}')
 
     dev = dev_path.replace('/dev/', '')
     path = Path(f'/sys/block/{dev}/size')
-    if not path:
-        print(f'{Colors.RED}unable to get size of {dev_path}{Colors.RESET}')
+    if not path.is_file():
+        print(f'{Colors.RED}unable to get size of {dev}{Colors.RESET}')
+        sys.exit(1)
 
-    size = 0
-    with path.open(mode='r', encoding=UTF8) as file:
-        size = 512 * file.read()
+    unit = config.get('unit')
+    for i, partition in enumerate(config.get('partitions')):
+        start = to_bytes(size=partition.get('start'), size_unit=unit)
+        end = partition.get('end')
+        if end == -1:
+            with path.open(mode='r', encoding=UTF8) as file:
+                end = 512 * int(file.read())
+        else:
+            end = to_bytes(size=end, size_unit=unit)
 
-    for i,partition in enumerate(config.get('partitions')):
-        print(f'Partition {i}\n'
+        size, unit = convert_size(size_bytes=end - start, size_unit=unit)
+        print(f'Partition {i + 1}\n'
               f'  Name: {Colors.BLUE}{partition.get("name")}{Colors.RESET}\n'
               f'  Filesystem: {Colors.BLUE}{partition.get("filesystem")}{Colors.RESET}\n'
               f'  Start: {Colors.BLUE}{partition.get("start")}{Colors.RESET}\n'
-              f'  End: {Colors.BLUE}{partition.get("end")}{Colors.RESET}')
+              f'  End: {Colors.BLUE}{partition.get("end")}{Colors.RESET}\n'
+              f'  Size: {Colors.BLUE}{size} {unit}{Colors.RESET}')
 
 
 def main() -> None:
