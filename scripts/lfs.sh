1e67387b8e1d9137702043b3833f4ddb8c1c4c00
Massimo Cannavo <massimo.wo6q2@simplelogin.co>
2023-05-21 17:59:35 -0400

    Added preparing build environment

--- scripts/lfs.sh
+++ scripts/lfs.sh
@@ -9,9 +9,18 @@ source utils.sh
 export LFS=/mnt/lfs
 
 is_root
-if ! grep -q "$LFS" /proc/mounts; then
+if ! grep -q $LFS /proc/mounts; then
   python mount.py
 fi
 
-mkdir -pv "${LFS}/sources"
-mkdir -pv "${LFS}/home"
+mkdir -pv $LFS/sources
+mkdir -pv $LFS/tools
+mkdir -pv $LFS/{etc,var} $LFS/usr/{bin,lib,sbin}
+for i in bin lib sbin; do
+  symlink=$LFS/$i
+  [[ ! -e $symlink ]] && ln -sv usr/$i $symlink
+done
+
+case $(uname -m) in
+  x86_64) mkdir -pv $LFS/lib64 ;;
+esac
