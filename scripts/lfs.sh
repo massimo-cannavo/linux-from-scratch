115acd899b20ca4a6d5b93ca42cb26bcfdb7b3bc
Massimo Cannavo <massimo.wo6q2@simplelogin.co>
2023-05-21 20:43:12 -0400

    Added more stuff to the build environment

--- scripts/lfs.sh
+++ scripts/lfs.sh
@@ -6,7 +6,13 @@
 set -e
 
 source utils.sh
+umask 022
+
 export LFS=/mnt/lfs
+export LFS_TGT=$ARCH-lfs-linux-gnu
+export LC_ALL=POSIX
+export PATH=$LFS/tools/bin:$PATH
+export CONFIG_SITE=$LFS/usr/share/config.site
 
 is_root
 if ! grep -q $LFS /proc/mounts; then
@@ -21,6 +27,12 @@ for i in bin lib sbin; do
   [[ ! -e $symlink ]] && ln -sv usr/$i $symlink
 done
 
-case $(uname -m) in
-  x86_64) mkdir -pv $LFS/lib64 ;;
+chown -v "$SUDO_USER" $LFS/tools
+chown -v "$SUDO_USER" $LFS/{etc,var,usr}
+chown -v "$SUDO_USER" $LFS/usr/{bin,lib,sbin}
+case $ARCH in
+  x86_64)
+    mkdir -pv $LFS/lib64
+    chown -v "$SUDO_USER" $LFS/lib64
+    ;;
 esac
