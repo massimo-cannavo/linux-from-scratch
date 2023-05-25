55cbd694dca86002e54d2b3949d6d02affae3cee
Massimo Cannavo <massimo.wo6q2@simplelogin.co>
2023-05-24 20:58:47 -0400

    Finished implementing download.py

--- scripts/download.py
+++ scripts/download.py
@@ -1,6 +1,8 @@
 '''Downloads a specific version of a package using a YAML file.'''
 import argparse
+import hashlib
 from pathlib import Path
+import sys
 
 from jsonschema import validate
 from jsonschema.exceptions import ValidationError
@@ -25,6 +27,12 @@ def main() -> None:
             pkg = yaml.safe_load(file)
             with Path(SCHEMA_FILE).open(mode='r', encoding=UTF8) as schema:
                 validate(pkg, yaml.safe_load(schema))
+
+            sha512 = download_pkg(url=pkg.get('source'), download_path=args.out)
+            if not sha512:
+                sys.exit()
+            if sha512 != pkg.get('checksum'):
+                handle_error(error=f'checksum verification failed for {pkg.get("name")}')
     except yaml.YAMLError as exc:
         if hasattr(exc, 'problem_mark'):
             error = f'{exc.problem}\n{exc.problem_mark}'
@@ -36,6 +44,8 @@ def main() -> None:
         handle_error(error=f'no such file or directory: {exc.filename}')
     except ValidationError as exc:
         handle_error(error=exc.message)
+    except requests.exceptions.RequestException as exc:
+        handle_error(error=exc)
 
 
 def parse_args() -> argparse.Namespace:
@@ -52,15 +62,32 @@ def parse_args() -> argparse.Namespace:
     return args
 
 
-def download_pkg(url: str, download_path: str) -> None:
-    '''TODO: add docstring.'''
-    pkg_file = f'{download_path}/{url.split("/")[-1]}'
+def download_pkg(url: str, download_path: str) -> str:
+    '''
+    Downloads and verifies the integrity of a package.
+
+    Parameters
+        url: str
+            URL used to download the package.
+        download_path: str
+            The path on the filesystem to download the package.
+    '''
+    pkg_file = url.split("/")[-1]
+    pkg_path = f'{download_path}/{pkg_file}'
+    if Path(pkg_path).exists():
+        print(f'{pkg_file} exists, skipping download')
+        return None
+
+    print(f'downloading {pkg_file} -> {download_path}')
     with requests.get(url, timeout=5, stream=True) as response:
         response.raise_for_status()
-        with open(pkg_file, mode='wb') as file:
-            for chunk in response.iter_content(chunk_size=8192):
+        sha512 = hashlib.sha512()
+        with open(pkg_path, mode='wb') as file:
+            for chunk in response.iter_content(chunk_size=100 * 1024**2):
+                sha512.update(chunk)
                 file.write(chunk)
 
+    return sha512.hexdigest()
 
 if __name__ == '__main__':
     main()
