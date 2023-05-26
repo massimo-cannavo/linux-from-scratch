e7b0f2ac8e86f5ee31803386d46dcd04b3ad3507
Massimo Cannavo <massimo.wo6q2@simplelogin.co>
2023-05-25 20:41:59 -0400

    Added extraction of package

--- scripts/download.py
+++ scripts/download.py
@@ -2,6 +2,7 @@
 import argparse
 import hashlib
 from pathlib import Path
+import subprocess
 import sys
 
 from jsonschema import validate
@@ -9,9 +10,9 @@ from jsonschema.exceptions import ValidationError
 import requests
 import yaml
 
-from partition import UTF8, handle_error
+from partition import UTF8, CommandNotFoundError, handle_error
 
-SOURCE_PATH=Path('/mnt/lfs/sources')
+SOURCE_PATH = '/mnt/lfs/sources'
 PARENT_DIR = Path(__file__).parent.parent
 SCHEMA_FILE = f'{PARENT_DIR}/package-schema.yaml'
 
@@ -19,20 +20,21 @@ SCHEMA_FILE = f'{PARENT_DIR}/package-schema.yaml'
 def main() -> None:
     '''The main entrypoint of the script.'''
     args = parse_args()
-    if not SOURCE_PATH.exists():
-        handle_error(error=f'no such file or directory: {SOURCE_PATH}')
-
     try:
         with Path(args.file).open(mode='r', encoding=UTF8) as file:
             pkg = yaml.safe_load(file)
             with Path(SCHEMA_FILE).open(mode='r', encoding=UTF8) as schema:
                 validate(pkg, yaml.safe_load(schema))
 
-            sha512 = download_pkg(url=pkg.get('source'), download_path=args.out)
+            url = pkg.get('source')
+            pkg_file = url.split('/')[-1]
+            sha512 = download_pkg(url, download_path=args.out)
             if not sha512:
                 sys.exit()
             if sha512 != pkg.get('checksum'):
                 handle_error(error=f'checksum verification failed for {pkg.get("name")}')
+
+            extract_pkg(pkg_path=f'{args.out}/{pkg_file}', out_path=args.out)
     except yaml.YAMLError as exc:
         if hasattr(exc, 'problem_mark'):
             error = f'{exc.problem}\n{exc.problem_mark}'
@@ -44,8 +46,10 @@ def main() -> None:
         handle_error(error=f'no such file or directory: {exc.filename}')
     except ValidationError as exc:
         handle_error(error=exc.message)
-    except requests.exceptions.RequestException as exc:
+    except (requests.exceptions.RequestException, CommandNotFoundError) as exc:
         handle_error(error=exc)
+    except subprocess.CalledProcessError as exc:
+        handle_error()
 
 
 def parse_args() -> argparse.Namespace:
@@ -88,6 +92,23 @@ def download_pkg(url: str, download_path: str) -> str:
                 file.write(chunk)
 
     return sha512.hexdigest()
+
+
+def extract_pkg(pkg_path: str, out_path: str) -> None:
+    '''
+    Extracts package that was downloaded.
+
+    Parameters
+        pkg_path: str
+            Path of the package to extract.
+        out_path: str
+            The path on the filesystem to extract the package.
+    '''
+    try:
+        print(f'extracting {pkg_path}')
+        subprocess.run(['tar', '-xvf', pkg_path, '-C', out_path], check=True)
+    except FileNotFoundError as exc:
+        raise CommandNotFoundError(f'command not found: {exc.filename}') from exc
 
 
 if __name__ == '__main__':
